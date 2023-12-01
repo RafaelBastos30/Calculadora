f6e26d36d0f8d5a2223af8d03f871d5eb67ed1f7
Rafael Bastos <rafael_bastos30@hotmail.com>
2023-11-30 21:23:06 -0300

    Finalizando calculadora

--- script.js
+++ script.js
@@ -1,4 +1,21 @@
 function tela(simb){
    var numero = document.getElementById('resultado').innerHTML;
-   
+   document.getElementById('resultado').innerHTML = numero + simb;
+}
+function limpa(){
+   document.getElementById('resultado').innerHTML = "";
+}
+function apaga(){
+   var apagar = document.getElementById('resultado').innerHTML;
+   document.getElementById('resultado').innerHTML = apagar.substring(0, apagar.length -1)
+}
+function calculo(){
+   var resultado = document.getElementById('resultado').innerHTML;
+   if(resultado){
+      document.getElementById('resultado').innerHTML = eval(resultado);
+   }
+   else{
+      document.getElementById('resultado').innerHTML = "Sem calculo";
+      
+   }
 }
